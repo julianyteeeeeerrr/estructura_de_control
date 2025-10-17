20cfd2eb546542ba17e2a343f230c3f644b0c5f5
julianyteeeeeerrr <carvallojulian53@gmail.com>
2025-10-17 16:04:22 -0500

    subida de ejercicios

--- estructuradecontrol/Program.cs
+++ estructuradecontrol/Program.cs
@@ -24,18 +24,76 @@
 
 //Ejercicio2
 
-Console.WriteLine("Ingrese el precio del producto: ");
-float precio = float.Parse(Console.ReadLine());
+//Console.WriteLine("Ingrese el precio del producto: ");
+//float precio = float.Parse(Console.ReadLine());
 
-if (precio >= 100000)
-{
-    float descuento = precio * 0.10f;
-    float total = precio - descuento;
-    Console.WriteLine("Se aplicó un 10% de descuento.");
-    Console.WriteLine("El precio final es: " + total);
-}
-else
-{
-    Console.WriteLine("No se aplica descuento.");
-    Console.WriteLine("El precio final es: " + precio);
-}
+//if (precio >= 100000)
+//{
+//    float descuento = precio * 0.10f;
+//    float total = precio - descuento;
+//    Console.WriteLine("Se aplicó un 10% de descuento.");
+//    Console.WriteLine("El precio final es: " + total);
+//}
+//else
+//{
+//    Console.WriteLine("No se aplica descuento.");
+//    Console.WriteLine("El precio final es: " + precio);
+//}
+
+//prestamo del estudiante
+
+//Console.WriteLine("Ingrese el monto del préstamo solicitado: ");
+//float monto = float.Parse(Console.ReadLine());
+
+//float interesAnual = monto * 0.05f; 
+//float interesTrimestre = interesAnual / 4;
+//float interesMes = interesAnual / 12;
+
+//float totalPagar = monto + (interesAnual * 5);
+
+//Console.WriteLine("interes pagado en un año:" + interesAnual);
+//Console.WriteLine("interes pagado en el tercer trimestre:" + interesTrimestre);
+//Console.WriteLine("interes pagado en el primer mes:" + interesMes);
+//Console.WriteLine("total a pagar incluyendo intereses: " + totalPagar);
+
+
+
+//algoritmo colilla
+
+//Console.WriteLine("Ingrese el salario del empleado: ");
+//float salario = float.Parse(Console.ReadLine());
+
+//Console.WriteLine("ingrese el valor del ahorro mensual programado: ");
+//float ahorro = float.Parse(Console.ReadLine());
+
+
+//float descuentoEPS = salario * 0.125f;
+//float descuentoPension = salario * 0.16f;
+//float totalDescuentos = descuentoEPS  + descuentoPension + ahorro;
+//float totalRecibir =  salario - totalDescuentos;
+
+//Console.WriteLine("salario del empleado: " + salario);
+//Console.WriteLine("Ahorro mensual: " + ahorro);
+//Console.WriteLine("Descuento EPS: " + descuentoEPS);
+//Console.WriteLine("Descuento Pensión: " + descuentoPension);
+//Console.WriteLine("Total a recibir: " + totalRecibir);
+
+
+//Matricula estudiante
+
+//Console.WriteLine("Ingrese el valor total de la matricula: ");
+//float matricula = float.Parse(Console.ReadLine());
+
+//float cuato1 = matricula * 0.40f;
+//float cuato2 = matricula * 0.25f;
+//float cuato3 = matricula * 0.20f;
+//float cuato4 = matricula * 0.15f;
+
+//Console.WriteLine("priemra cuota (40%): " + cuato1);
+//Console.WriteLine("segunda cuota (25%): " + cuato2);
+//Console.WriteLine("tercera cuota (20%): " + cuato3);
+//Console.WriteLine("cuarta cuota (15%): " + cuato4);
+
+
+
+
--- estructuradecontrol/obj/Debug/net8.0/estructuradecontrol.AssemblyInfo.cs
+++ estructuradecontrol/obj/Debug/net8.0/estructuradecontrol.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("estructuradecontrol")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3e5f82a80e131ef886fc8e128fb2d36031f7bed1")]
 [assembly: System.Reflection.AssemblyProductAttribute("estructuradecontrol")]
 [assembly: System.Reflection.AssemblyTitleAttribute("estructuradecontrol")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
